6e4f10327b86a22f5af9094ab7b2c7a990cc00b5
Nick Kossolapov <nickkossolapov@gmail.com>
2021-07-27 07:32:12 +0100

    Add expiring cache (#7)
    * Add expiring cache store
    
    * Filter out all expired items when fetching cache values
    
    * Expire cache when fetching values instead of filter

---
+++ src/Cosmos/Store.fs
@@ -0,0 +1,49 @@
+namespace Cosmos.Store
+
+open System
+open System.Collections.Generic
+open Cosmos.Types
+open Cosmos.Prelude.Result
+
+type ExpiringCache<'a>() =
+    let cache = Dictionary<string, DateTime * 'a>()
+
+    member _.Add (TimeToLive ttl) id item =
+        let validUntil = DateTime.UtcNow.AddHours(float ttl)
+
+        let addToCache () =
+            match cache.TryAdd(id, (validUntil, item)) with
+            | true -> Ok cache.[id]
+            | false -> Error StoreError
+            |> Result.map snd
+
+        let handleExisting expiry =
+            if expiry < DateTime.UtcNow then
+                cache.Remove id |> ignore
+                addToCache ()
+            else
+                Error AlreadyExistsError
+
+        match cache.TryGetValue id with
+        | true, (expiry, _) -> handleExisting expiry
+        | false, _ -> addToCache ()
+
+    member _.Get id =
+        let handleExisting expiry value =
+            if expiry < DateTime.UtcNow then
+                cache.Remove id |> ignore
+                Error ExpiredError
+            else
+                Ok value
+
+        match cache.TryGetValue id with
+        | true, (expiry, value) -> handleExisting expiry value
+        | false, _ -> Error NotFoundError
+
+    member _.Values() =
+        for KeyValue(id, (expiry, _)) in cache do
+            if expiry < DateTime.UtcNow then
+                cache.Remove id |> ignore
+        
+        cache.Values
+        |> Seq.map snd
---
+++ test/Cosmos.Tests/StoreTests.fs
@@ -0,0 +1,72 @@
+namespace Cosmos.Tests.StoreTests
+
+open Xunit
+open Cosmos.Prelude.Result
+open Cosmos.Types
+open Cosmos.Store
+
+module ExpiringCacheTests =
+    [<Fact>]
+    let ``Can store and retrieve item from expiring cache`` () =
+        let cache = ExpiringCache()
+        let ttl = TimeToLive 1<hr>
+        cache.Add ttl "1" "test" |> ignore
+
+        let item = cache.Get "1"
+
+        let expected = Ok "test"
+
+        Assert.Equal(expected, item)
+
+    [<Fact>]
+    let ``Cannot retrieve expired item from expiring cache`` () =
+        let cache = ExpiringCache()
+        let ttl = TimeToLive -1<hr>
+        cache.Add ttl "1" "test" |> ignore
+
+        let item = cache.Get "1"
+
+        let expected = Error ExpiredError
+
+        Assert.Equal(expected, item)
+
+    [<Fact>]
+    let ``Cannot add to expiring cache when non-expired item exists`` () =
+        let cache = ExpiringCache()
+        let ttl = TimeToLive 1<hr>
+        cache.Add ttl "1" "test" |> ignore
+
+        let item = cache.Add ttl "1" "this already exists"
+
+        let expected = Error AlreadyExistsError
+
+        Assert.Equal(expected, item)
+
+    [<Fact>]
+    let ``Can add to expiring cache when expired item exists`` () =
+        let cache = ExpiringCache()
+        let ttl = TimeToLive -1<hr>
+        cache.Add ttl "1" "test" |> ignore
+
+        let newTtl = TimeToLive 1<hr>
+        let item = cache.Add newTtl "1" "new item"
+
+        let expected = Ok "new item"
+
+        Assert.Equal(expected, item)
+
+    [<Fact>]
+    let ``Can get non-expired items from expiring cache`` () =
+        let cache = ExpiringCache()
+        let ttl = TimeToLive 1<hr>
+        cache.Add ttl "1" "test1" |> ignore
+        cache.Add ttl "2" "test2" |> ignore
+        
+        let oldTtl = TimeToLive -1<hr>
+        cache.Add oldTtl "3" "expired item" |> ignore
+
+        let items = cache.Values ()
+
+        let expected = [ "test1"; "test2" ]
+
+        Assert.Equal(expected, items)
